4e6b35c2fa085bcc85a51c7cefbfa131523572c0
EthanHueh <nattanssouzapvz@gmail.com>
2024-12-14 11:59:31 -0300

    Melhorias na Tela Simulador de Combate

--- src/main/java/toolsforrpg_panpalianos/gui/telas/TelaSimuladorDeCombate.java
+++ src/main/java/toolsforrpg_panpalianos/gui/telas/TelaSimuladorDeCombate.java
@@ -1,80 +1,87 @@
 package toolsforrpg_panpalianos.gui.telas;
 
-import java.util.List;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import toolsforrpg_panpalianos.dados.modelo.fichas.Ficha;
-import toolsforrpg_panpalianos.dados.repositorios.FichasRepository;
-import toolsforrpg_panpalianos.dominio.servicos.GeradorMensagens;
 import toolsforrpg_panpalianos.dominio.servicos.simulador_de_combate.Personagem;
 import toolsforrpg_panpalianos.dominio.servicos.simulador_de_combate.SimuladorDeCombate;
+import toolsforrpg_panpalianos.gui.componentes.SelecaoUtils;
+import toolsforrpg_panpalianos.gui.componentes.botoes.BotaoPadrao;
 import toolsforrpg_panpalianos.gui.telas.comum.TelaAviso;
 import toolsforrpg_panpalianos.gui.telas.comum.TelaErro;
 import toolsforrpg_panpalianos.gui.telas.comum.TelaInput;
 
-public class TelaSimuladorDeCombate {
+public class TelaSimuladorDeCombate extends JFrame {
 
     private SimuladorDeCombate simCom = new SimuladorDeCombate();
 
+    private JComboBox<Object> selecionarFichaJogador = new JComboBox<>();
+    private JComboBox<Object> selecionarFichaInimigo = new JComboBox<>();
+
+    public TelaSimuladorDeCombate(){
+        SelecaoUtils.mudarParaTodasAsFichas(selecionarFichaJogador);
+        SelecaoUtils.mudarParaTodasAsFichas(selecionarFichaInimigo);
+
+        setTitle("Simulador de Combate");
+        setLayout(new BorderLayout());
+        add(new JLabel("Bem vindo ao simulador de combate"), BorderLayout.NORTH);
+        
+        JPanel painelPrincipal = new JPanel();
+        painelPrincipal.setLayout((new GridLayout(2,2)));
+        
+        painelPrincipal.add(new JLabel("Jogador"));
+        painelPrincipal.add(selecionarFichaJogador);
+        painelPrincipal.add(new JLabel("Inimigo"));
+        painelPrincipal.add(selecionarFichaInimigo);
+
+        JButton btn = new BotaoPadrao("Iniciar");
+        btn.addActionListener(
+            e -> confirmarCombate()
+        );
+
+        add(btn, BorderLayout.SOUTH);
+
+        add(painelPrincipal);
+
+        pack();
+
+    }
+
     public void iniciar(){
+        SelecaoUtils.mudarParaTodasAsFichas(selecionarFichaJogador);
+        SelecaoUtils.mudarParaTodasAsFichas(selecionarFichaInimigo);
         
-        try {
-            List<Ficha> fichas = FichasRepository.retornarTodasAsFichas();
+        setVisible(true);
+    }
 
-            Personagem jogador = simCom.getJogador();
-            Personagem inimigo = simCom.getInimigo();
-            
-            String msg = GeradorMensagens.gerarMensagemFichasMenu(fichas);
+    private void confirmarCombate(){
+        Ficha fJogador = (Ficha) selecionarFichaJogador.getSelectedItem();
+        Ficha fInimigo = (Ficha) selecionarFichaInimigo.getSelectedItem();
 
-            TelaAviso.avisar("Bem vindo ao simulador de combate!", "SIMULADOR DE COMBATE");
-            
-            do {
-                int opcao = TelaInput.obterInteiro(msg+"\nQuem você quer que seja o jogador?","Jogador");
+        String msg = "Jogador: "+fJogador.getNome()+"\nInimigo: "+fInimigo.getNome();
 
-                if (opcao > 0 && opcao <= fichas.size()){
-                    jogador.setFicha(fichas.get(opcao - 1));
-                    break;
-                }
-
-                if (opcao == fichas.size() + 1){
-                    if(TelaInput.desejaSair()){
-                        return;
-                    }
-                }
+        if (TelaInput.desejaRealizarOperacao("Deseja iniciar combate?\n\n"+msg, "Combate")){
+            if (fJogador == fInimigo){
+                TelaErro.mostrar("As fichas precisam ser diferentes!");
+                return;
             }
-            while (true);
 
-            do {
-                int opcao = TelaInput.obterInteiro(msg+"\nQuem você quer que seja o inimigo?","Inimigo");
+            simCom.getJogador().setFicha(fJogador);;
+            simCom.getInimigo().setFicha(fInimigo);
 
-                if (opcao > 0 && opcao <= fichas.size()){
-                    inimigo.setFicha(fichas.get(opcao - 1));
-                    
-                    if (inimigo.getFicha() == jogador.getFicha()){
-                        TelaErro.mostrar("As fichas precisam ser diferentes!", "Fichas iguais");
-                        continue;
-                    }
-                    break;
-                }
-
-                if (opcao == fichas.size() + 1){
-                    if(TelaInput.desejaSair()){
-                        return;
-                    }
-                }
-        
-            }
-            while (true);
-
-            TelaAviso.avisar(jogador.getFicha().getNome()+" X "+inimigo.getFicha().getNome(),"Combate!");
-            
-            executar();
-        } catch (Exception e) {
-            TelaErro.mostrar(e);
+            TelaAviso.avisar(fJogador.getNome()+" X "+fInimigo.getNome(),"Combate!");
+            executarCombate();
         }
-        
     }
     
-    private void executar() {
+    private void executarCombate() {
 
         Personagem jogador = simCom.getJogador();
         Personagem inimigo = simCom.getInimigo();
@@ -114,12 +121,12 @@ public class TelaSimuladorDeCombate {
 
     private String msgJogador(Personagem jogador) {
         return new StringBuilder()
-            .append("O que fazes, "+jogador.getFicha().getNome()+" ?")
+            .append("O que fazes, "+jogador.getFicha().getNome()+"?\n")
             .append("1 - Atacar\n")
             .append("2 - Defender\n")
             .append("3 - Desviar\n")
             .append("4 - Usar pocao\n")
-            .append("5 - Habilidade Classe ("+jogador.getHabilidadeClasse()+")")
+            .append("5 - Habilidade Classe ("+jogador.getHabilidadeClasse()+")\n")
             .append("6 - Sair")
         .toString();
     }
