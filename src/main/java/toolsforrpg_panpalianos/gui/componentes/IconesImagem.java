3f3ec6b85427893997f01254a219e781094dd7ab
EthanHueh <nattanssouzapvz@gmail.com>
2025-01-30 17:52:52 -0300

    Adição de icones

--- src/main/java/toolsforrpg_panpalianos/gui/componentes/IconesImagem.java
+++ src/main/java/toolsforrpg_panpalianos/gui/componentes/IconesImagem.java
@@ -12,7 +12,11 @@ public enum IconesImagem {
     LOGO("src/main/resources/images/capa_old_dragon.jpg"),
     ARMA("src/main/resources/images/icones/espada.png",20,20),
     ARMADURA("src/main/resources/images/icones/armaduras.png",20,20),
-    ESCUDO("src/main/resources/images/icones/escudo.png",20,20);
+    ESCUDO("src/main/resources/images/icones/escudo.png",20,20),
+    
+    OLHO("src/main/resources/images/icones/olho-aberto.png",20,20),
+    EDITAR("src/main/resources/images/icones/botao-editar.png",20,20),
+    EXCLUIR("src/main/resources/images/icones/lixo.png",20,20);
 
     private ImageIcon imageIcon;
 
